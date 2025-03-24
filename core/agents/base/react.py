d99325f7a7155eaab57e96fdb747872528d1cb3f
“lgesuellip” <lgesuellipinto@uade.edu.ar>
2025-03-24 18:47:12 -0300

    add sse

--- core/agents/base/react.py
+++ core/agents/base/react.py
@@ -1,20 +1,12 @@
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
-
-from mcp import StdioServerParameters
-from mcp import ClientSession, StdioServerParameters
-
-from mcp.client.stdio import stdio_client
-
-from langchain_mcp_adapters.tools import load_mcp_tools
-
-from langchain_arcade import ArcadeToolManager
+from contextlib import asynccontextmanager
+from langchain_mcp_adapters.client import MultiServerMCPClient
 
 import asyncio
 import logging
 
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,49 +16,34 @@ logger.setLevel(logging.INFO)
 
 logger.info("Starting the example client script.")
 
-server_params = StdioServerParameters(
-    command="uv",
-    args= [
-        "--directory",
-        "/Users/lgesuellip/Desktop/mcp_firecrawl/researcher_agent/servers",
-        "run",
-        "website-firecrawl-service"
-    ]
-)
 
-async def main():
+@asynccontextmanager
+async def build_agent():
 
     tools = []
-
-    # Get tools from MCP
-    async with stdio_client(server_params) as (read, write):
-        async with ClientSession(read, write) as session:
-            await session.initialize()
-            tools.extend(await load_mcp_tools(session))
+    async with MultiServerMCPClient(
+        {
+            "slack": {
+                "url": "https://actions.zapier.com/mcp/sk-ak-WXJtdTT4eJntUOY8OlsIDYYaSJ/sse",
+                "transport": "sse",
+            },
+            "researcher": {
+                "command": "uv",
+                "args": [
+                    "--directory",
+                    "/Users/lgesuellip/Desktop/mcp_firecrawl/researcher_agent/servers",
+                    "run",
+                    "website-firecrawl-service"
+                ],
+                "transport": "stdio"
+            }
+        }
+    ) as client:
     
-            # Get tools from Arcade
-            tool_arcade_manager = ArcadeToolManager(api_key=os.getenv("ARCADE_API_KEY"))
-            tools.extend(tool_arcade_manager.get_tools(toolkits=["slack"]))
-        
+            tools = client.get_tools()
             model = ChatOpenAI(model="gpt-4")
 
             graph = create_react_agent(model, tools=tools)
-
-            inputs = {
-                "messages": [HumanMessage(content="A summary of pampa.ai website, and send it to 'lautaro'")],
-            }
-
-            config = {
-                "configurable": {
-                    "thread_id": "1",
-                    "user_id": os.getenv("ARCADE_USER_ID"),
-                }
-            }
-            result = await graph.ainvoke(inputs, config=config)
-            print(result)
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-
+            
+            yield graph
 
