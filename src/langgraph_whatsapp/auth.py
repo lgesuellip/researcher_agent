abb4d8e88665aef2728b2b11dc30f445541de4da
“lgesuellip” <lgesuellipinto@uade.edu.ar>
2025-04-12 12:03:49 -0300

    fix whatsapp class

--- src/langgraph_whatsapp/auth.py
+++ src/langgraph_whatsapp/auth.py
@@ -2,7 +2,10 @@ from langgraph_sdk import Auth
 
 auth = Auth()
 
-# TODO: Improve this function
 @auth.authenticate
 async def authenticate(request, path, headers, method):
+    # Add production-grade auth into your LangGraph deployments, 
+    # no other backend or proxy required. This is the middleware
+    # that will be used to authenticate the request.
+    # ex  {"identity": "default-user", "permissions": ["read", "write"]}
     return None
